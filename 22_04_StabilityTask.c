5eb9cc79b924c1c8ba07f1d1cb497886e7b25f13
DevashishDhyani <ddhy609@aucklanduni.ac.nz>
2020-04-23 00:02:14 +1200

    22nd night

--- 22_04_StabilityTask.c
+++ 22_04_StabilityTask.c
@@ -24,17 +24,13 @@
 #define   TASK_STACKSIZE       2048
 
 // Definition of Task Priorities
-#define PRINT_STATUS_TASK_PRIORITY 14
-#define GETSEM_TASK1_PRIORITY      13
-#define GETSEM_TASK2_PRIORITY      12
-#define RECEIVE_TASK1_PRIORITY    11
-#define RECEIVE_TASK2_PRIORITY    10
-#define SEND_TASK_PRIORITY        9
+#define SEVEN_SEG_TASK_PRIORITY 1  //seven seg related
 
-#define COUNTER_TASK_PRIORITY 6
+#define SWITCHES_TASK_PRIORITY 3
+#define PRINT_STATUS_TASK_PRIORITY 2
+#define UPDATE_LOAD_TASK_PRIORITY 5
+#define STABILITY_TASK_PRIORITY 6
 
-
-#define SWITCHES_TASK_PRIORITY 1
 
 //LCD Macros
 FILE *lcd;
@@ -45,46 +41,51 @@ FILE *lcd;
 #define   MSG_QUEUE_SIZE  30
 QueueHandle_t msgqueue;
 
+#define FREQ_QUEUE_SIZE  10
+QueueHandle_t freqQueue;
+
+
 // used to delete a task
 TaskHandle_t xHandle;
 
 // Definition of Semaphore
-SemaphoreHandle_t shared_resource_sem;
 SemaphoreHandle_t shared_lcd_sem;
 SemaphoreHandle_t shared_sevenseg_sem;
+SemaphoreHandle_t shared_freq_sem;
 
 //Definition of Timer
-TimerHandle_t timer_switches;
 
 
 // globals variables
-unsigned int number_of_messages_sent = 0;
-unsigned int number_of_messages_received_task1 = 0;
-unsigned int number_of_messages_received_task2 = 0;
-unsigned int getsem_task1_got_sem = 0;
-unsigned int getsem_task2_got_sem = 0;
-char sem_owner_task_name[20];
 
 //threshold via KB
 int thresholdFreq = 50;
 char tempThresholdFreq[4] = "";
 
-//global for state toggle
+//state toggle
 int stateToggle = 0; //0 = maintenance mode, 1 = freq relay mode
 
+//frequency related
+double freqArray[FREQ_QUEUE_SIZE];
+double rocArray[FREQ_QUEUE_SIZE];
+int freqIndex = 0;
+int freqIndexNew = 1;
+
+//debug variables
+unsigned int debugCounter =0;
 
 // Local Function Prototypes
 int initOSDataStructs(void);
 int initCreateTasks(void);
 
 
-
 void freq_relay(){
 	unsigned int temp = IORD(FREQUENCY_ANALYSER_BASE, 0);
-	//printf("%f Hz\n", 16000/(double)temp);
+	double tempFreq = 16000/(double) temp;
+	//printf("%f Hz in being SENT \n", 16000/(double)temp);
+	xQueueSendToFrontFromISR(freqQueue, &tempFreq, pdFALSE);
 	return;
 }
-
 
 void ps2_isr (void* context, alt_u32 id)
 {
@@ -167,8 +168,6 @@ void ps2_isr (void* context, alt_u32 id)
   }
 }
 
-
-// first we write our interrupt function
 void button_interrupts_function(void* context, alt_u32 id)
 {
   // need to cast the context first before using it
@@ -185,43 +184,71 @@ void button_interrupts_function(void* context, alt_u32 id)
   IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PUSH_BUTTON_BASE, 0x7);
 }
 
-void vTimerSwitches(xTimerHandle t_timer){ //Timer flashes green LEDs
-	//IOWR_ALTERA_AVALON_PIO_DATA(GREEN_LEDS_BASE, 0xFF^IORD_ALTERA_AVALON_PIO_DATA(GREEN_LEDS_BASE));
-
-	unsigned int uiSwitchValue = 0;
-    // read the value of the switch and store to uiSwitchValue
-    uiSwitchValue = IORD_ALTERA_AVALON_PIO_DATA(SLIDE_SWITCH_BASE);
-    // write the value of the switches to the red LEDs
-    IOWR_ALTERA_AVALON_PIO_DATA(RED_LEDS_BASE, uiSwitchValue);
-}
-
+//need to enable writing to a queue for this
 void switches_task(void *pvParameters)
 {
-	//TIMER Switches
-	timer_switches = xTimerCreate("Timer Switches", 1000, pdTRUE, NULL, vTimerSwitches);
-
-	if (xTimerStart(timer_switches, 0) != pdPASS){
-		printf("Cannot start timer");
-	}
-
 	while (1)
 	{
-		//figure out a way to make sure this while loops isn't
-		//causing busy waiting.
-		//vTaskDelay(100);
+		unsigned int uiSwitchValue = 0;
+		// read the value of the switch and store to uiSwitchValue
+		uiSwitchValue = IORD_ALTERA_AVALON_PIO_DATA(SLIDE_SWITCH_BASE);
+		// write the value of the switches to the red LEDs
+		IOWR_ALTERA_AVALON_PIO_DATA(RED_LEDS_BASE, uiSwitchValue);
+
+		vTaskDelay(200);
 	}
 }
 
-// For exercise in lab
-void counter_task(void *pvParameters)
+//need to follow a pattern to calc RoC, threshold check and overwriting old values
+void stability_task(void *pvParameters)
+{
+	while (1)
+	{
+		xQueueReceive(freqQueue, freqArray+freqIndex, portMAX_DELAY); // Pops queue, blocks indefinitely if empty
+
+		xSemaphoreTake(shared_freq_sem, portMAX_DELAY);
+
+		//printf("%d\n", debugCounter);
+		//printf("%f Hz in being RECEIVED\n", freqArray[freqIndex]);
+		//debugCounter++;
+
+		//find ROC
+
+
+		freqIndex = freqIndex+1;  //next element to be written to
+		if(freqIndex >= FREQ_QUEUE_SIZE) {
+			freqIndex = 0;
+		}
+
+		freqIndexNew = freqIndexNew+1;
+		if(freqIndexNew >= FREQ_QUEUE_SIZE) {
+			freqIndexNew = 0;
+		}
+
+		xSemaphoreGive(shared_freq_sem);
+
+		//maybe some sort of ulTaskNotifiy for LoadManager might be needed
+	}
+}
+
+//empty atm
+void update_load_task(void *pvParameters)
+{
+	while (1)
+	{
+		//maybe some sort of ulTaskNotifiy for LoadManager might be needed
+	}
+}
+
+//printing group name for now
+void seven_seg_task(void *pvParameters)
 {
 	while (1)
 	{
 		xSemaphoreTake(shared_sevenseg_sem, portMAX_DELAY);
 		IOWR_ALTERA_AVALON_PIO_DATA(SEVEN_SEG_BASE, 19);
 		xSemaphoreGive(shared_sevenseg_sem);
-
-		vTaskDelay(1000);
+		vTaskDelay(10000);
 	}
 }
 
@@ -246,82 +273,6 @@ void print_status_task(void *pvParameters)
 		//insert some code to release the semophore
 		xSemaphoreGive(shared_lcd_sem);
 
-		vTaskDelay(1000);
-		//printf("****************************************************************\n");
-	}
-}
-
-// The next two task compete for a shared resource via a semaphore.  The name of
-// the task that owns the semaphore is copied into the global variable
-// sem_owner_task_name[].
-
-void getsem_task1(void *pvParameters)
-{
-	while (1)
-	{
-		xSemaphoreTake(shared_resource_sem, portMAX_DELAY);
-		// block forever until receive the mutex
-		strcpy(&sem_owner_task_name[0], "getsem_task1");
-		getsem_task1_got_sem++;
-		xSemaphoreGive(shared_resource_sem);
-		vTaskDelay(100);
-	}
-}
-
-void getsem_task2(void *pvParameters)
-{
-	while (1)
-	{
-		xSemaphoreTake(shared_resource_sem, portMAX_DELAY);
-		// block forever until receive the mutex
-		strcpy(&sem_owner_task_name[0], "getsem_task2");
-		getsem_task2_got_sem++;
-		xSemaphoreGive(shared_resource_sem);
-		vTaskDelay(130);
-	}
-}
-
-// The following task fills up a message queue with incrementing data.  The data
-// is not actually used by the application.  If the queue is full the task is
-// suspended for 1 second.
-void send_task(void *pvParameters)
-{
-	unsigned int msg = 0;
-	while (1)
-	{
-		if (xQueueSend(msgqueue, (void *)&msg, 0) == pdPASS)
-		{
-			// in the message queue
-			msg++;
-			number_of_messages_sent++;
-		}
-		else
-		{
-			vTaskDelay(1000);
-		}
-	}
-}
-
-// The next two task pull messages in the queue at different rates.  The number
-// of messages received by the task is incremented when a new message is received
-void receive_task1(void *pvParameters)
-{
-	unsigned int *msg;
-	while (1)
-	{
-		xQueueReceive(msgqueue, &msg, portMAX_DELAY);
-		number_of_messages_received_task1++;
-		vTaskDelay(333);
-	}
-}
-
-void receive_task2(void *pvParameters)
-{
-	unsigned int *msg;
-	while (1)
-	{
-		xQueueReceive(msgqueue, &msg, portMAX_DELAY);
-		number_of_messages_received_task2++;
 		vTaskDelay(1000);
 	}
 }
@@ -377,30 +328,30 @@ int main(int argc, char* argv[], char* envp[])
 // This function simply creates a message queue and a semaphore
 int initOSDataStructs(void)
 {
+	freqQueue = xQueueCreate(FREQ_QUEUE_SIZE, sizeof(double));
+
 	msgqueue = xQueueCreate( MSG_QUEUE_SIZE, sizeof( void* ) );
-	shared_resource_sem = xSemaphoreCreateCounting( 9999, 1 );
 	shared_lcd_sem = xSemaphoreCreateCounting( 9999, 1 );
+
 	shared_sevenseg_sem = xSemaphoreCreateCounting( 9999, 1 );
+	shared_freq_sem = xSemaphoreCreateCounting( 9999, 1 );
 	return 0;
 }
 
 // This function creates the tasks used in this example
 int initCreateTasks(void)
 {
-	xTaskCreate(getsem_task1, "getsem_task1", TASK_STACKSIZE, NULL, GETSEM_TASK1_PRIORITY, NULL);
-	xTaskCreate(getsem_task2, "getsem_task2", TASK_STACKSIZE, NULL, GETSEM_TASK2_PRIORITY, NULL);
-	xTaskCreate(receive_task1, "receive_task1", TASK_STACKSIZE, NULL, RECEIVE_TASK1_PRIORITY, NULL);
-	xTaskCreate(receive_task2, "receive_task2", TASK_STACKSIZE, NULL, RECEIVE_TASK2_PRIORITY, NULL);
-	xTaskCreate(send_task, "send_task", TASK_STACKSIZE, NULL, SEND_TASK_PRIORITY, NULL);
 	xTaskCreate(print_status_task, "print_status_task", TASK_STACKSIZE, NULL, PRINT_STATUS_TASK_PRIORITY, NULL);
-	xTaskCreate(counter_task, "counter_task", TASK_STACKSIZE, NULL, COUNTER_TASK_PRIORITY, NULL);
+	xTaskCreate(seven_seg_task, "seven_seg_task", TASK_STACKSIZE, NULL, SEVEN_SEG_TASK_PRIORITY, NULL);
 
 	xTaskCreate(switches_task, "switches_task", TASK_STACKSIZE, NULL, SWITCHES_TASK_PRIORITY, NULL);
 
+	xTaskCreate(stability_task, "stability_task", TASK_STACKSIZE, NULL, STABILITY_TASK_PRIORITY, NULL);
+
+	//xTaskCreate(update_load_task, "update_load_task", TASK_STACKSIZE, NULL, UPDATE_LOAD_TASK_PRIORITY, NULL);
+
 	return 0;
 }
-
-
 
 
 //Helper Functions
